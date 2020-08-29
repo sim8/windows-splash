9bf9f7cb626a7a897fbdee95747227d0bdd2b9b3
sim8 <simeon.k.lees@gmail.com>
2020-08-29 16:09:35 +0100

    Initial blog setup

---
+++ pages/blog.tsx
@@ -0,0 +1,40 @@
+import format from 'date-fns/format';
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+import { BlogPost, getPosts } from '../utils/posts';
+
+export async function getStaticProps() {
+  const sortedPosts = getPosts().sort((a, b) => a.date.localeCompare(b.date));
+  return { props: { posts: sortedPosts } };
+}
+
+export default function Home({ posts }: { posts: BlogPost[] }) {
+  return (
+    <main className="page-content" aria-label="Content">
+      <div className="wrapper">
+        <div className="home">
+          <Head>
+            <title>timer.blog</title>
+          </Head>
+          <h1 className="page-heading">timer.blog</h1>
+          <h2 className="post-list-heading">Posts</h2>
+          <ul className="post-list">
+            {posts.map(post => (
+              <li key={post.slug}>
+                <span className="post-meta">
+                  {format(new Date(post.date), 'MMM d, yyyy')}
+                </span>
+                <h3>
+                  <Link href="/blog/[slug]" as={`/blog/${post.slug}`}>
+                    <a className="post-link">{post.title}</a>
+                  </Link>
+                </h3>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,88 @@
+import format from 'date-fns/format';
+import hl from 'highlight.js';
+import marked from 'marked';
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+import { getPosts } from '../../utils/posts';
+
+type PostProps = {
+  slug: string;
+  title: string;
+  date: string;
+  html: string;
+};
+
+export const getStaticPaths = () => ({
+  paths: getPosts().map(p => `/blog/${p.slug}`),
+  fallback: false,
+});
+
+export function getStaticProps({
+  params,
+}: {
+  params: { slug: string };
+}): { props: PostProps } {
+  const { slug } = params;
+
+  const post = getPosts().find(p => p.slug === slug)!;
+  const { title, date, content } = post;
+  return {
+    props: {
+      slug,
+      title,
+      date,
+      html: marked(content, {
+        highlight: function (code, lang) {
+          return hl.highlight(lang, code).value;
+        },
+      }),
+    },
+  };
+}
+
+export default function Post(props: PostProps) {
+  const { slug, title, date, html } = props;
+  return (
+    <main className="page-content" aria-label="Content">
+      <div className="wrapper">
+        <article className="post h-entry">
+          <header className="post-header">
+            <Head>
+              <title>{title}</title>
+            </Head>
+            <Link href="/blog">
+              <a>&laquo; Back</a>
+            </Link>
+            <h1 className="post-title p-name" itemProp="name headline">
+              {title}
+            </h1>
+            <p className="post-meta">
+              <time
+                className="dt-published"
+                dateTime={date}
+                itemProp="datePublished"
+              >
+                {format(new Date(date), 'MMM d, yyyy')}
+              </time>
+            </p>
+          </header>
+
+          <div
+            className="post-content e-content"
+            itemProp="articleBody"
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></div>
+
+          <a className="u-url" href={`/blog/${slug}`} hidden></a>
+
+          <footer className="site-footer">
+            <Link href="/">
+              <a>&laquo; Back</a>
+            </Link>
+          </footer>
+        </article>
+      </div>
+    </main>
+  );
+}
